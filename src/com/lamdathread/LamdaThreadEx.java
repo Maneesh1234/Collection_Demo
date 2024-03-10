c2bba2d673557ed7a01f49b914fb1cad1b289ba1
Maneesh1234 <51262723+Maneesh1234@users.noreply.github.com>
2024-03-10 10:22:05 +0530

    Update LamdaThreadEx.java

--- src/com/lamdathread/LamdaThreadEx.java
+++ src/com/lamdathread/LamdaThreadEx.java
@@ -12,7 +12,7 @@ public class LamdaThreadEx {
 		Thread t1=new Thread(r1);
 		t1.start();
 		
-		//Thread without lambda
+		//Thread with lambda
 		Runnable r2=()->{System.out.println("Thread 2 is running");};
 		Thread t2=new Thread(r2);
 		t2.start();
